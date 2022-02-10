bc85a0c0def69af0e7037db403464844f59d9322
Shuo Tian <tianshuo1996@outlook.com>
2022-02-10 14:41:21 -0600

    remove swiper

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,7 @@
-import { Container } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { Contact } from "../components/Home/Contact";
 import { Me } from "../components/Home/Me";
-import { Slides } from "../components/Home/Slides";
 import { Work } from "../components/Home/Work";
 interface Props {}
 
@@ -13,17 +11,9 @@ const Home: NextPage<Props> = () => {
       <Head>
         <title>Shuo Tian</title>
       </Head>
-      <Slides>
-        <Container maxW="container.lg">
-          <Me />
-        </Container>
-        <Container maxW="container.lg">
-          <Work />
-        </Container>
-        <Container maxW="container.lg">
-          <Contact />
-        </Container>
-      </Slides>
+      <Me />
+      <Work />
+      <Contact />
     </>
   );
 };
