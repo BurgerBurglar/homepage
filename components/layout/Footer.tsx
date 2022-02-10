02b6f7074e144a6c8b249b3c2a4aae2fd5c43401
Shuo Tian <tianshuo1996@outlook.com>
2022-02-10 14:51:16 -0600

    css adjust

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Footer: React.FC = () => {
   return (
-    <Flex color="gray.300" bgColor="#ffffff22" flex="2rem 0 0 ">
+    <Flex color="gray.300" bgColor="#ffffff22" h="3rem">
       <Container>
         <Center h="full">© Shuo Tian 2022, no rights reserved</Center>
       </Container>
