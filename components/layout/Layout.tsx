b3deca6b2d0e2a12298acc22cba24e6cd06e07ff
Shuo Tian <tianshuo1996@outlook.com>
2022-02-11 22:57:00 -0600

    navbar responsive design

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -4,12 +4,10 @@ import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
 export const Layout: React.FC = ({ children }) => {
-  const navbarH = "3rem";
-  const bgColor = useColorModeValue("#ffffffBB", "#000000BB");
   return (
     <>
       <Box>
-        <Navbar h={navbarH} />
+        <Navbar />
         <Box className="content">{children}</Box>
         <Footer />
       </Box>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,16 +1,36 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
 import {
+  Box,
+  Button,
   Container,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   FlexProps,
   HStack,
+  IconButton,
+  Input,
   Link,
   LinkProps,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Select,
-  Spacer,
+  Stack,
+  useBoolean,
+  useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import NextLink from "next/link";
-import { ReactElement } from "react";
+import { ReactElement, useRef } from "react";
+import { useBreakPoint } from "../../utils/useBreakPoint";
 
 interface NavLinkProps extends LinkProps {
   href: string;
@@ -26,47 +46,102 @@ const NavLink: React.FC<NavLinkProps> = ({ href, content, ...linkProps }) => (
 interface NavbarProps extends FlexProps {}
 export const Navbar: React.FC<NavbarProps> = (props) => {
   const color = "white";
-  return (
-    <Flex
-      as="nav"
-      position="fixed"
-      zIndex={9}
-      w="full"
-      align="center"
-      bgColor="nav"
-      fontWeight="bold"
-      {...props}
-    >
-      <Container maxW="container.lg" display="flex" alignItems="center">
-        <HStack spacing="5rem" color={color} fontSize="1.2rem" w="full">
-          <NavLink
-            href="/home"
-            h="36px"
-            content={
-              <Image src="/favicon.ico" alt="logo" width={36} height={36} />
-            }
-          />
-          <NavLink href="/blogs" content="blogs" />
-          <NavLink href="/" content="retro" />
-          <Spacer />
+  const isMobile = useBreakPoint(768);
 
-          <Select
-            variant="unstyled"
-            defaultValue="en"
-            fontWeight="bold"
-            w="fit-content"
-            sx={{
-              option: {
-                bgColor: "nav",
-                color: color,
-              },
-            }}
+  const [isOpen, { toggle, off }] = useBoolean();
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  const icon = (
+    <NavLink
+      href="/home"
+      h="36px"
+      content={<Image src="/favicon.ico" alt="logo" width={36} height={36} />}
+    />
+  );
+
+  const navLinks = [
+    <NavLink key={0} href="/blogs" content="blogs" />,
+    <NavLink key={1} href="/" content="retro" />,
+  ];
+
+  const selection = (
+    <Select
+      variant="unstyled"
+      defaultValue="en"
+      fontWeight="bold"
+      w="8em"
+      ml="auto"
+      sx={{
+        option: {
+          bgColor: "nav",
+          color: color,
+        },
+      }}
+    >
+      <option value="en">English</option>
+      <option value="zh-CN">简体中文</option>
+    </Select>
+  );
+
+  return (
+    <>
+      <Flex
+        as="nav"
+        bgColor="nav"
+        color={color}
+        direction="column"
+        position="fixed"
+        zIndex={9}
+        w="full"
+      >
+        <Container maxW="container.lg">
+          <Flex
+            minH="3rem"
+            justifyContent="space-between"
+            align="center"
+            fontSize="1.2rem"
+            {...props}
           >
-            <option value="en">English</option>
-            <option value="zh-CN">简体中文</option>
-          </Select>
-        </HStack>
-      </Container>
-    </Flex>
+            {isMobile ? (
+              <>
+                <IconButton
+                  ref={btnRef}
+                  icon={<HamburgerIcon />}
+                  aria-label="drawer"
+                  colorScheme="blue"
+                  bgColor="nav"
+                  position="relative"
+                  left={-4}
+                  onClick={toggle}
+                />
+                {icon}
+              </>
+            ) : (
+              <>
+                <HStack spacing="clamp(1rem, 10vw, 5rem)" w="full">
+                  {[icon, navLinks]}
+                </HStack>
+                {selection}
+              </>
+            )}
+          </Flex>
+          {!isMobile || !isOpen ? null : (
+            <VStack spacing={4} align="start" py={4}>
+              {[navLinks, selection]}
+            </VStack>
+          )}
+        </Container>
+      </Flex>
+      {!isMobile || !isOpen ? null : (
+        <Box
+          className="overlay"
+          position="fixed"
+          w="100vw"
+          h="100vh"
+          zIndex={3}
+          onClick={off}
+        />
+      )}
+    </>
   );
 };
